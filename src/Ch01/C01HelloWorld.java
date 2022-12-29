401ddb68799567f6d4e6b2706b017057223eca8a
minsikkims <wozn159@nate.com>
2022-12-29 17:06:31 +0900

    Ch02

---
+++ src/Ch01/C01HelloWorld.java
@@ -0,0 +1,14 @@
+package Ch01; // 패키지명(폴더와 유사)
+
+public class C01HelloWorld  // 클래스 영역 - 객체 지향 문법 적용 단위
+{
+	// 메서드(방법, 기능) : 작업 수행을 하는데 필요한 여러 코드를 묶어서 실행하는 데 사용되는 단위
+	// main 메서드      : 최초 실행 함수
+	// library 메서드   : 미리 만들어져서 제공되는 함수
+	// 사용자 정의 메서드  : 개발자에 의해 만들어지는 함수
+	public static void main(String[] args)  // 메서드 영역 - 절차 지향 문법 적용 단위
+	{
+		System.out.println("HELLOW WORLD");
+	}
+
+}
